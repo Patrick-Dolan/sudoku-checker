40fd64f4dc8047d60911839cf5f8141198bf1191
Patrick Dolan <dolanp1992@gmail.com>
2021-12-15 20:21:11 -0800

    Co-authored-by: Phil Curran <XLCM@users.noreply.github.com> Co-authored-by: Wajma Niazi <WNiazi@users.noreply.github.com>

--- __test__/sudokuchecker.test.js
+++ __test__/sudokuchecker.test.js
@@ -12,8 +12,24 @@ describe("Sudoku", () => {
     sudoku.addColumns();
     expect(sudoku.columns[0]).toEqual([5, 6, 0, 8, 4, 7, 0, 0, 0]);
   });
+  // test("should not repeat numbers 1-9 in a row", ()=> {
+  //   sudoku.repeatNumberRow(); 
+  //   expect(sudoku.rows[0]).toBe(false);
+  // });
   test("should correctly create a grid of 3x3", () => {
     sudoku.addGrids();
-    expect(sudoku.grids[0]).toEqual([5, 6, 0, 6, 0, 0, 0, 9, 8]);
+    expect(sudoku.grids[0]).toEqual([5, 3, 0, 6, 0, 0, 0, 9, 8]);
   });
+  test("should transform first three rows into three 3x3 grids and return first grid", () => {
+    sudoku.addGrids();
+    expect(sudoku.grids[0]).toEqual([5, 3, 0, 6, 0, 0, 0, 9, 8]);
+  });
+  test("should transform first three rows into three 3x3 grids and return second grid", () => {
+    sudoku.addGrids();
+    expect(sudoku.grids[1]).toEqual([0, 7, 0, 1, 9, 5, 0, 0, 0]);
+  });
+  // test("should transform first three rows into three 3x3 grids and return first grid", () => {
+  //   sudoku.addGrids();
+  //   expect(sudoku.grids[0]).toEqual([5, 3, 0, 6, 0, 0, 0, 9, 8]);
+  // });
 });
